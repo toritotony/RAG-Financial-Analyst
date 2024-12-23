6de00c79ff374073eba34ecddf7638d55f78a7ee
toritotony <wolfeanthony584@gmail.com>
2024-12-23 12:10:01 -0800

    remove cached files

--- scheduledtask.bat
+++
@@ -1,4 +0,0 @@
-"C:\Users\wolfe\AppData\Local\Programs\Python\Python311\python.exe" -m pip install --upgrade pip setuptools wheel beautifulsoup4 requests langchain langchain_openai langchain_pinecone pinecone python-dotenv 
-"C:\Users\wolfe\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\wolfe\OneDrive\Desktop\stock-analyst-tool-llamaindex\webscrape.py"
-"C:\Users\wolfe\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\wolfe\OneDrive\Desktop\stock-analyst-tool-llamaindex\vectorstore.py"
-pause
