62f69bf9a6bfb7dbf8ca33583b9d6e654753d74b
toritotony <wolfeanthony584@gmail.com>
2025-02-02 20:51:54 -0800

    Update example_webscrape.py
    Change element being retrieved, use regex to find all similar elements that are serialized, then check for nested anchor element  to make sure it's a valid article reference. Add sleep function at the end to space http requests sent out.

--- example_webscrape.py
+++ example_webscrape.py
@@ -1,16 +1,22 @@
 from bs4 import BeautifulSoup as BS
+import time
 import requests as rq
+import re
 
 headers = {'User-Agent': 'Mozilla/5.0'}  
-url = "https://www.investing.com/news/"
+url = "https://www.investing.com"
 webpage = rq.get(url, headers=headers)
 trav = BS(webpage.content, "html.parser")
-articles = trav.find_all('article', {'data-test': 'article-item'})
-articles_data = []
+articles = trav.find_all('div', {'data-test': re.compile('homepage-news-list-item')})
+articles_data = [] 
 
 for article in articles:
-    headline = article.find('a', {'data-test': 'article-title-link'}).text.strip()
-    link = article.find('a', {'data-test': 'article-title-link'})['href']
+    a_tag = article.find('a')
+    if a_tag is None:
+        print("No anchor tag found, skipping...")
+        continue
+    headline = a_tag.text.strip()
+    link = a_tag['href']
     full_link = 'https://www.investing.com' + link if not link.startswith('http') else link
     # Fetch the article page
     article_page = rq.get(full_link, headers=headers)
@@ -25,3 +31,5 @@ file_path = 'path to where your articles.txt will be located locally'
 with open(file_path, 'a', encoding='utf-8') as f:
     for article in articles_data:
        f.write(article['link'] + '\n')
+
+time.sleep(5)
